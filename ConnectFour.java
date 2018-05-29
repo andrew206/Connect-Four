94f487e2932d95425659c6f32b739e86b45d5882
andrew206 <39346435+andrew206@users.noreply.github.com>
2018-05-29 11:09:57 -0700

    Update ConnectFour.java

--- ConnectFour.java
+++ ConnectFour.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.util.*;
+import java.io.*;
 
 public class ConnectFour {
    public static void main(String[] args) {
@@ -7,11 +9,35 @@ public class ConnectFour {
       frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       frame.setSize(670, 600);
       frame.setTitle("Connect Four");
-      
       GUI panel = new GUI();
       panel.setBackground(Color.BLUE);
       frame.add(panel);
-      
       frame.setVisible(true);
-   }
+      
+      GUI GUI = new GUI();
+      
+      Scanner console = new Scanner(System.in);
+      boolean win = false;
+      int turn = 1;
+      
+      Graphics g = panel.getGraphics();
+      
+      while (!win) {
+          if (turn%2 != 0) {
+              System.out.println("Red's Move:");
+          } else {
+              System.out.println("Yellow's Move:");
+          }
+          
+          System.out.print("x: ");
+          int x = console.nextInt() - 1;
+          
+          System.out.print("y: ");
+          int y = console.nextInt() - 1;
+          System.out.println();
+          
+          turn++;
+          GUI.drawTile(x, y, turn, g);
+      }
+   } 
 }
