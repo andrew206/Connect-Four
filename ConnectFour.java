ccee26c49588bd63ef59a1f303145ceeabfe49a9
andrew206 <39346435+andrew206@users.noreply.github.com>
2018-06-03 13:07:11 -0700

    Add files via upload

---
+++ ConnectFour.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class ConnectFour extends GUI {
+   public static void main(String[] args) {
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(670, 600);
+      frame.setTitle("Connect Four");
+      GUI panel = new GUI();
+      panel.setBackground(Color.BLUE);
+      frame.add(panel);
+      frame.setVisible(true);
+      
+      GUI GUI = new GUI();
+      
+      Scanner console = new Scanner(System.in);
+      boolean win = false;
+      int turn = 1;
+           
+      Graphics g = panel.getGraphics();
+      int[] y = new int[7]; 
+      String[][] grid = new String[6][7];
+      for (int i = 0; i < 6; i++) {
+         for (int j = 0; j < 7; j++) { 
+            grid[i][j] = "empty";
+         }
+      }
+      
+      while (!win) {       
+          if (turn%2 != 0) {
+              System.out.println("Red's Move");
+          } else {
+              System.out.println("Yellow's Move");
+          }
+          
+          System.out.print("Column (1-7) : ");
+          int x = console.nextInt() - 1;
+          
+          System.out.println();
+          y[x]++;
+          turn++;
+          grid[y[x] - 1][x] = GUI.drawTile(x, y, turn, g);   
+          //System.out.println(Arrays.deepToString(test));   
+          win = Win.win(grid);            
+      }
+   }
+ }
---
+++ GUI.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.io.*;
+
+public class GUI extends JPanel {
+   public void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      g.setColor(Color.WHITE);
+      for (int i = 0; i < 7; i++) {
+         for (int j = 0; j < 6; j++) {         
+            g.fillOval(i * 90 + 20, 90 + 20, 70, 70);  
+            g.fillOval(i * 90 + 20, j * 90 + 20, 70, 70); 
+         }      
+      }      
+   }
+   
+   public String drawTile(int x, int[] y, int turn, Graphics g) {
+      String color = "";
+      if (turn%2 != 0) {
+         g.setColor(Color.YELLOW);
+         color = "yellow";
+      } else {
+         g.setColor(Color.RED);
+         color = "red";
+      }  
+      g.fillOval(x * 90 + 20, 470 - (y[x] - 1) * 90, 70, 70); 
+      return color;    
+   }
+}
